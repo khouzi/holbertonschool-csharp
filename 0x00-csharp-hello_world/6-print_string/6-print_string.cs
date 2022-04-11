4974800e55c020ed4060512d4a10d6bdbae148f4
khouzi <khouzem.boussaid@gmail.com>
2022-04-11 22:11:30 +0200

    6-print_string/

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+        string t = new StringBuilder(str.Length * 3).Insert(0, str, 3).ToString();
+        string k = str.Substring(0, 9);
+		Console.WriteLine("{0}\n{1}", t, k);
+        }
+}
