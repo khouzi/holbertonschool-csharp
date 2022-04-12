fdb45457efc6eb27101520cd5dff904ae81eaecd
khouzi <khouzem.boussaid@gmail.com>
2022-04-12 15:49:27 +0200

    8-print_last_digit.cs

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _8_print_last_digit
+{
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            int last_digit;
+
+            last_digit = number % 10;
+            Console.Write(last_digit);
+            return(last_digit);
+        }
+    }
+}
