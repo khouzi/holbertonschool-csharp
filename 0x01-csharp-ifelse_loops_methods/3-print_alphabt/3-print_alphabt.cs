7a589860764830c67b0ce0d7bc2b3f3d914bcd05
khouzi <khouzem.boussaid@gmail.com>
2022-04-12 13:13:39 +0200

    3-print_alphabt/

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char i;
+
+            for (i = 'a'; i < 'z'; i++)
+                if (i != 'q' && i != 'e')
+                    Console.Write(i);
+        }
+    }
+}
