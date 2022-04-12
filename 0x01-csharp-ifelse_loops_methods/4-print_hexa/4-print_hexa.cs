4aa11f11a3b7cce61580441a2f4ceb42d7d1ac64
khouzi <khouzem.boussaid@gmail.com>
2022-04-12 12:48:00 +0200

    4-print_hexa/

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for(i = 0; i <= 98; i++)
+            {
+                    Console.Write("{0} = 0x{0:x}\n",i ,i);
+            }
+        }
+    }
+}
