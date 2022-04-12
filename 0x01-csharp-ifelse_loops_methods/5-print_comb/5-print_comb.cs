83ff5122373e382e62751d3ad28d06f5628e137c
khouzi <khouzem.boussaid@gmail.com>
2022-04-12 13:11:48 +0200

    5-print_comb/

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for(i = 0; i <=98; i++)
+                Console.Write("{0:D2}, ", i);
+            Console.Write("{0}\n", i);
+        }
+    }
+}
