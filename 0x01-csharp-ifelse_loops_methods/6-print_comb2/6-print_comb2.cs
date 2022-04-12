4fde76be335a40f497e23ad0abd458c8e36ef24f
khouzi <khouzem.boussaid@gmail.com>
2022-04-12 14:51:59 +0200

    6-print_comb2/

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+
+            for(i = 0; i <=9; i++)
+            {
+                for(j = 0; j <= 8; j++)
+                {
+                    if (i < j)
+                    {
+                        Console.Write(i);
+                        Console.Write("{0}, ",j);
+                    }
+                }
+            }
+            Console.WriteLine("79, 89");
+        }
+    }
+}
