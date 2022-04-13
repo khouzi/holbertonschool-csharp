3855b1e5947b4c42ffe2e4854e943b7f4530eefa
khouzi <khouzem.boussaid@gmail.com>
2022-04-13 15:30:51 +0200

    6-divisible_by_2.cs

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<bool> DivisibleBy2(List<int> myList)
+        {
+            List<bool> nlist = new List<bool>();
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (myList[i] % 2 == 0)
+                {
+                    nlist.Add(true);
+                }
+                else
+                    nlist.Add(false);
+            }
+            return nlist;
+        }
+    }
