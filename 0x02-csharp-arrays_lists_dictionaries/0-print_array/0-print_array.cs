92221fbcfc6d62840e0e40b5269d52feb53e6495
khouzi <khouzem.boussaid@gmail.com>
2022-04-13 12:57:47 +0200

    0-print_array/

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size == 0)
+            {
+                Console.WriteLine();
+                return new int[size];
+            }
+            else if (size < 0)
+            {
+                Console.WriteLine("Size connot be negative");
+                return null;    
+            }
+
+            else
+            {
+                int[] array = new int[size];
+                
+                for (int i = 0; i < size; i++)
+                {
+                    array[i] = i;
+                    if (i < size -1)
+                    Console.Write(array[i] + " ");
+                    else
+                    Console.Write(array[i]);
+                }
+                Console.WriteLine();
+                return array;
+            }
+
+
+        }
+    }
