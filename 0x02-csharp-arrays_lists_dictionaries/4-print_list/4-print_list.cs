0b306a74a37cfd9107cbd1acbf90caafc31f5cce
khouzi <khouzem.boussaid@gmail.com>
2022-04-13 15:02:13 +0200

    4-print_list/

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            System.Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> numbers = new List<int>();
+        if ( size > 0)
+        {    
+            int i = 0;
+        
+            for (; i < size - 1; i++)
+            {
+                numbers.Add(i);
+                System.Console.Write("{0} ",i);
+            }
+
+            numbers.Add(i);
+            System.Console.WriteLine(i);
+        }
+        else
+            System.Console.WriteLine();
+        return numbers;
+    }
+}
