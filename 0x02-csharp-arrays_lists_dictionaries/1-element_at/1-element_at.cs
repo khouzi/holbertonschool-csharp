ff66774694b6b70be124c477460ab06432612f2d
khouzi <khouzem.boussaid@gmail.com>
2022-04-13 13:23:26 +0200

    1-element_at/

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -5,7 +5,7 @@
     {
         public static int elementAt(int[] array, int index)
         {
-            if (index > array.Length || index < 0)
+            if (index > array.Length - 1 || index < 0)
             {
                 Console.WriteLine("Index out of range");
                 return (-1);
