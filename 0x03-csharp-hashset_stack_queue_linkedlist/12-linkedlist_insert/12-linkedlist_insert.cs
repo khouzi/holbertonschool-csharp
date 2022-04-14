81785d7b2963fb7e0cd7aaf573309b0a9644f55a
khouzi <khouzem.boussaid@gmail.com>
2022-04-14 20:47:30 +0200

    fix

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        if (myLList.Count == 0)
+        {
+            myLList.AddFirst(n);
+            return myLList.First;
+        }
+        LinkedListNode<int> current = myLList.First;
+
+        while(current != null)
+        {
+            if (n <= current.Value)
+            {
+                myLList.AddBefore(current, n);
+                return current.Previous;
+            }
+            if (current.Next != null)
+                current = current.Next;
+            else
+                break;
+        }
+
+        myLList.AddAfter(current, n);
+        return current.Next;
+
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int i = 0;
+        foreach(var element in myLList)
+        {
+            if (element == value)
+            {
+                return (i);
+            }
+            i++;
+        }
+        return -1;
+    }
+}
