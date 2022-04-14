68a3157ff6fb1fb9548ac2b18f34f93158570121
khouzi <khouzem.boussaid@gmail.com>
2022-04-14 20:42:39 +0200

    10-linkedlist_get_node/

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int i = 0;
+        foreach (var element in myLList)
+        {
+            if (i == n)
+                return element;
+            i++;
+        }
+        return 0;
+    }
+}
