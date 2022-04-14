2898431c77b56a439ded703e78b13f4659196201
khouzi <khouzem.boussaid@gmail.com>
2022-04-14 16:11:54 +0200

    1-common_elements/

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> nlist= new List<int>();
+
+        foreach (var item in list1)
+        {
+            if (list2.Contains(item))
+            {
+                nlist.Add(item);
+            }
+        }
+        return nlist;
+    }
+}
