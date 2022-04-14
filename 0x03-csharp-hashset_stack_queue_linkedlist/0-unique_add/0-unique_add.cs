dbb9b921bed9b8f0828bb1da0a8d6dcef928d3f4
khouzi <khouzem.boussaid@gmail.com>
2022-04-14 15:32:51 +0200

    0-unique_add.cs

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            HashSet<int> nelement = new HashSet<int>();
+            foreach(int element in myList)
+            {
+                nelement.Add(element);
+            }
+            int a = 0;
+            foreach(int item in nelement)
+                {
+                    a += item;
+                }
+            return a;
+
+        }
+    }
+
