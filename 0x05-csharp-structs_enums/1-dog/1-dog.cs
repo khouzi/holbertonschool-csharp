bbf710f0b6e7e6b93ec454a325bcaf6fafbabb6f
khouzi <khouzem.boussaid@gmail.com>
2022-04-25 19:31:45 +0200

    1-dog.cs

---
+++ 0x05-csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+enum Rating
+    {
+        Good,
+        Great,
+        Excellent,
+    }
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
+
