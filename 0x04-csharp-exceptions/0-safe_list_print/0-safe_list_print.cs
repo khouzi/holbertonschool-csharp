386ba7777af9ec48d89fdaffb6b642b3cb3d727b
khouzi <khouzem.boussaid@gmail.com>
2022-04-25 17:31:24 +0200

    0-safe_list_print/

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int SafePrint(List<int> myList, int n)
+        {
+            int  counter = 0;
+            try
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine(myList[i]);
+                    counter++;
+                }
+            }
+            catch
+            {
+               return counter;
+            }
+            return counter;
+        }
+    }
+
