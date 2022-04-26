861edef2b6acf9c9a6a4a3d574adb0eac91d85ee
khouzi <khouzem.boussaid@gmail.com>
2022-04-26 19:25:55 +0200

    3-enemy.cs

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// zombie class
+    /// </summary>
+    class Zombie 
+    {
+        
+        private int health;
+        /// <summury> public constructor </summury>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summury> zombie health checker </summury>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                this.health = value;
+            }
+            else
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+        public int GetHealth()
+        {
+            return this.health;
+        }
+    }
+}
