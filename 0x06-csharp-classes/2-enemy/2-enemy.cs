2b69b1d0a0617209000e7c170428334dc4fb9a58
khouzi <khouzem.boussaid@gmail.com>
2022-04-26 19:17:20 +0200

    2-enemy.cs

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// zombie class
+    /// </summary>
+    class Zombie 
+    {
+        public int health;
+
+        /// <summury> public constructor </summury>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summury> zombie health checker </summury>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater tahn or equal to 0");
+            }
+        }
+    }
+}
