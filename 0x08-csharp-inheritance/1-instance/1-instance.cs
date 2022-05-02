b2bae9e109b585e357d61faf701f1dd0a6a1444d
khouzi <khouzem.boussaid@gmail.com>
2022-05-02 02:51:30 +0200

    1-instance.cs

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Obj
+    {
+        public static bool IsInstanceOfArray(object obj)
+        {
+            if (obj is Array || obj.GetType() == typeof(Array))
+                return true;
+            else
+                return false;
+        }
+    }
