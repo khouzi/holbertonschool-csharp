d9167cb613106df6fb00d5d937e472b5091fa146
khouzi <khouzem.boussaid@gmail.com>
2022-05-18 14:53:24 +0200

    9-vector_scalar_mul.cs

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] A = new double[2];
+		double[] B = new double[3];
+		double[] C = new double[]{-	1};
+
+        if ( vector.Length == 2)
+        {
+            A[0] = vector[0] * scalar;
+            A[1] = vector[1] * scalar;
+            return A;
+        }
+        if ( vector.Length == 3)
+        {
+            B[0] = vector[0] * scalar;
+            B[1] = vector[1] * scalar;
+            return B;
+        }
+    }    
+}
