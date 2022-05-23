d29b71310ba55fb1db9be4a78bc3150eedf447b4
khouzi <khouzem.boussaid@gmail.com>
2022-05-23 12:16:54 +0200

    loop change with a fix

--- 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -4,24 +4,11 @@ class VectorMath
 {
     public static double[] Multiply(double[] vector, double scalar)
     {
-        double[] A = new double[2];
-		double[] B = new double[3];
-		double[] C = new double[]{-	1};
-
-        if ( vector.Length == 2)
-        {
-            A[0] = vector[0] * scalar;
-            A[1] = vector[1] * scalar;
+        if (vector.Length != 3 && vector.Length != 2)
+                return new Double[1]{-1};
+            double[] A = new Double[3];
+            for(int i = 0; i < vector.Length; i++)
+                   A[i] = scalar * vector[i];
             return A;
-        }
-        else if ( vector.Length == 3)
-        {
-            B[0] = vector[0] * scalar;
-            B[1] = vector[1] * scalar;
-            return B;
-        }
-
-        else
-            return C;
     }    
 }
