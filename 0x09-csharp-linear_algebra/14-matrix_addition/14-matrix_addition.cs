d6451b2193503d63398a187285233162908e2728
khouzi <khouzem.boussaid@gmail.com>
2022-05-23 12:53:47 +0200

    14-matrix_addition.cs

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -6,16 +6,17 @@ class MatrixMath
 {
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        double[,] A = new double[2,2];
-		double[,] B = new double[3,3];
+        double[,] A = new double[3,3];
 
         if (matrix1.Length != matrix2.Length && matrix1.Length != 3 || matrix1.Length != 2 )
                 return new double[1,1]{{-1}};
 
                 
         for(int i = 0; i < matrix1.GetLength(0); i++)
+        {
             for (int j = 0; j < matrix1.GetLength(1); j++)
                     A[i,j] = matrix1[i,j] + matrix2[i,j];
+        }
         return new double[1,1]{{-1}};
     }
 }
