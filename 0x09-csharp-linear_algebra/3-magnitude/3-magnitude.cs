dfbae81028f111aed4e1f1da95ad61a68e6fca6e
khouzi <khouzem.boussaid@gmail.com>
2022-05-23 12:05:53 +0200

    3-magnitude.cs

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -7,21 +7,17 @@
     {
         public static double Magnitude(double[] vector)
         {
-        /// <summary>
-        /// Calculates a given vector magnitude
-        /// </summary>
-        /// <param name="vector">the vector</param>
-        /// <returns>Magntiude of the vector</returns>
+            double x = 0;
+
             if (vector.Length !=3 && vector.Length !=2)
                 return -1;
 
             else if (vector.Length == 2)  
-                return (Math.Round(Math.Sqrt((Math.Pow(vector[0], 2) + (Math.Pow(vector[1], 2))))));
+                x = (Math.Sqrt((Math.Pow(vector[0], 2) + (Math.Pow(vector[1], 2)))));
 
             else if (vector.Length == 3)
-                return (Math.Round(Math.Sqrt((Math.Pow(vector[0], 2) + (Math.Pow(vector[1], 2)) + (Math.Pow(vector[2], 2)))), 2));
-            
-            
-            
+                x = Math.Round(Math.Sqrt((Math.Pow(vector[0], 2) + (Math.Pow(vector[1], 2)) + (Math.Pow(vector[2], 2)))));
+
+            return x ;      
         }
     }
