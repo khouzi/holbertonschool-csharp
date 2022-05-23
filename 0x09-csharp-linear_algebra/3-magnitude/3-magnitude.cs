fe245b5e0b8d220fe04debc5274d105007ac52d8
khouzi <khouzem.boussaid@gmail.com>
2022-05-23 12:09:23 +0200

    3-magnitude.cs

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -13,11 +13,11 @@
                 return -1;
 
             else if (vector.Length == 2)  
-                x = (Math.Sqrt((Math.Pow(vector[0], 2) + (Math.Pow(vector[1], 2)))));
+                x = Math.Round(Math.Sqrt((Math.Pow(vector[0], 2) + (Math.Pow(vector[1], 2)))), 2);
 
             else if (vector.Length == 3)
-                x = Math.Sqrt((Math.Pow(vector[0], 2) + (Math.Pow(vector[1], 2)) + (Math.Pow(vector[2], 2)))));
+                x = Math.Round(Math.Sqrt((Math.Pow(vector[0], 2) + (Math.Pow(vector[1], 2)) + (Math.Pow(vector[2], 2)))), 2);
 
-            return Math.Round(x, 2) ;      
+            return x ;      
         }
     }
