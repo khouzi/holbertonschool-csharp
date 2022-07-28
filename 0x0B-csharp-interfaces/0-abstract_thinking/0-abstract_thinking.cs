cdfb42699b698183a4231aa299d4c78a61c61a60
khouzi <khouzem.boussaid@gmail.com>
2022-07-28 19:16:28 +0200

    0-abstract_thinking.cs

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+/// <summary>
+/// obstract class
+/// </summary>
+abstract class Base
+{
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} is a {GetType().Name}";
+    }
+}
+
