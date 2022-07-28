7737b9b31f1ec1e3477cce515303a342491cbafe
khouzi <khouzem.boussaid@gmail.com>
2022-07-28 20:34:14 +0200

    2-doors.cs

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,83 @@
+﻿using System;
+/// <summary>
+/// obstract class
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// string name
+    /// </summary>
+    /// <value></value>
+    public string name { get; set; }
+    /// <summary>
+    /// overide ToString()
+    /// </summary>
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
+/// <summary>
+/// interface IIdentifiable
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// interact()
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// interface IBreakable
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>
+    /// int
+    /// </summary>
+    int durability { get; set; }
+    /// <summary>
+    /// method Break()
+    /// </summary>
+    void Break();
+}
+
+/// <summary>
+/// interface ICollectable
+/// </summary>
+public interface ICollectable
+{
+    /// <summary>
+    /// bool isCollected
+    /// </summary>
+    bool isCollected { get; set; }
+    /// <summary>
+    /// collect()
+    /// </summary>
+    void Collect();
+}
+
+/// <summary>
+/// class Door
+/// </summary>
+public class Door : Base, IInteractive
+{
+
+    /// <summary>
+    /// constructor
+    /// </summary>
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+    /// <summary>
+    /// interact()
+    /// </summary>
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
+
+
