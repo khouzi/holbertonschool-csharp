16a48c075ab063bd8957224dbc381b4f9a03b3d5
khouzi <62845331+khouzi@users.noreply.github.com>
2022-07-28 10:30:38 -0700

    fix

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -7,9 +7,6 @@ abstract class Base
     public string name { get; set; }
 
     public override string ToString()
-    /// <summary>
-    /// override ToString()
-    /// <summary>
     {
         return $"{name} is a {GetType().Name}";
     }
