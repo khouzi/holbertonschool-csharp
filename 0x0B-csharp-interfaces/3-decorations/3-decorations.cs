b700330135482f74e7143231fbdab1b638683e77
khouzi <khouzem.boussaid@gmail.com>
2022-07-29 13:43:09 +0200

    3-decorations.cs

--- 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
+++ 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
@@ -117,7 +117,7 @@ public class Decoration : Base, IInteractive, IBreakable
         if (durability <= 0)
             Console.WriteLine($"The {name} has been broken.");
         else if (isQuestItem == true)
-            Console.WriteLine($"you look at the {name}. There's a key inside.");
+            Console.WriteLine($"You look at the {name}. There's a key inside.");
         else
             Console.WriteLine($"You look at the {name}. Not much to see here.");
     }
