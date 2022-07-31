d58cfc99d2014991a762c1e28cebb158a39f1840
khouzi <khouzem.boussaid@gmail.com>
2022-07-31 03:27:48 +0200

    1-damage_delegation.cs

---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+/// <summary>
+/// class Player
+/// </summary>
+public class Player
+{
+    string name;
+    float maxHp;
+    float hp;
+
+    /// <summary>
+    /// player constructor
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxHp"></param>
+    public Player(string name ="Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp <= 0f)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.hp = this.maxHp;
+    }
+
+    /// <summary>
+    /// print health
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+    /// <summary>
+    /// delegate for taking damage
+    /// </summary>
+    /// <param name="damage"></param>
+    public delegate void CalculateHealth(float damage);
+
+    /// <summary>
+    /// TakeDamage
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0f)
+        {
+            damage = 0f;
+            Console.WriteLine($"{name} takes no damage!");
+        }
+        else
+            Console.WriteLine($"{name} takes {damage} damage!");
+    }
+
+    /// <summary>
+    /// HealDamage
+    /// </summary>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0f)
+        {
+            Console.WriteLine($"{name} heals 0 HP!");
+            heal = 0f;
+        }
+        else
+            Console.WriteLine($"{name} heals {heal} HP!");
+    }
+}
